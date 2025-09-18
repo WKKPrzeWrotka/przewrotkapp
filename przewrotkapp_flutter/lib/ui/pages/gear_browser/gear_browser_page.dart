d02659d48d2fc5eccd9d0cb2692d618a70bf8d4e
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-09-18 10:18:21 +0200

    all gear page

--- przewrotkapp_flutter/lib/ui/pages/gear_browser/gear_browser_page.dart
+++ przewrotkapp_flutter/lib/ui/pages/gear_browser/gear_browser_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:przewrotkapp_client/przewrotkapp_client.dart';
 
-class GearBrowserPage extends StatelessWidget {
+import '../../../data_types.dart';
+import '../../../gear_search.dart';
+import '../../common/gear_listing.dart';
+import '../../common/gear_search_filters.dart';
+
+class GearBrowserPage extends StatefulWidget {
   const GearBrowserPage({super.key});
 
   @override
+  State<GearBrowserPage> createState() => _GearBrowserPageState();
+}
+
+class _GearBrowserPageState extends State<GearBrowserPage> {
+  var filteredGear = <GearPair>[];
+
+  @override
   Widget build(BuildContext context) {
-    return Text("this page was shit :P so i destroyed it");
+    final allGear = context.watch<AllGearCache?>();
+    final favs = context.watch<UserFavourites?>()?.gearIds ?? [];
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            expandedHeight: 300,
+            title: Text("Cały sprzęt"),
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              titlePadding:
+                  EdgeInsets.only(top: kToolbarHeight, left: 8, right: 8),
+              expandedTitleScale: 1,
+              title: GearSearchFilters(
+                onFiltersChange: ({text, types}) {
+                  filteredGear = sortGear(
+                    searchGear(allGear ?? [], text: text, types: types),
+                    favs,
+                  );
+                  setState(() {});
+                },
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, i) => GearListing(gearPair: filteredGear[i]),
+              childCount: filteredGear.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- przewrotkapp_flutter/lib/ui/pages/home/favourite_gear_card.dart
+++ przewrotkapp_flutter/lib/ui/pages/home/favourite_gear_card.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:przewrotkapp_client/przewrotkapp_client.dart';
-import '../../common/gear_listing.dart';
 
 import '../../../data_types.dart';
+import '../../common/gear_listing.dart';
 
 class FavouriteGearCard extends StatefulWidget {
   const FavouriteGearCard({super.key});
@@ -28,7 +29,7 @@ class _FavouriteGearCardState extends State<FavouriteGearCard> {
             for (final gear in favGear ?? <GearPair>[])
               GearListing(gearPair: gear),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => context.push('/gear'),
               child: Text("Przeglądaj cały sprzęt"),
             ),
           ],
