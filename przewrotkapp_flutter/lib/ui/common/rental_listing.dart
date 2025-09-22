629923f39ce5706d3ea0f9b91791cb1c731ab4b5
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-09-22 23:35:30 +0200

    actually rented gear

--- przewrotkapp_flutter/lib/ui/common/rental_listing.dart
+++ przewrotkapp_flutter/lib/ui/common/rental_listing.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:przewrotkapp_client/przewrotkapp_client.dart';
 import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 
+import '../../data_types.dart';
+import 'gear_listing.dart';
 import 'utils.dart';
 
 class RentalListing extends StatelessWidget {
@@ -12,20 +15,29 @@ class RentalListing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final allGear = context.watch<AllGearCache?>();
     // THIS IS UGLY AS FUCk
-    // TODO: All RentalGroup stuff
     return Card(
-      child: ListTile(
-        onTap: () => context.push('/user/${rental.userInfoId}'),
-        title: Text("${rental.from.toStringDate(showYear: false)} do "
-            "${rental.to.toStringDate(showYear: false)}"),
-        subtitle: Row(
-          spacing: 4,
-          children: [
-            CircularUserImage(userInfo: rental.userInfo),
-            Text(rental.userInfo!.userName ?? ""),
-          ],
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            onTap: () => context.push('/user/${rental.userInfoId}'),
+            title: Text(
+              "${rental.from.toStringDate(showYear: false)} do "
+              "${rental.to.toStringDate(showYear: false)}",
+            ),
+            subtitle: Row(
+              spacing: 4,
+              children: [
+                CircularUserImage(userInfo: rental.userInfo),
+                Text(rental.userInfo!.userName ?? ""),
+              ],
+            ),
+          ),
+          if (allGear != null)
+            for (final gear in rental.gearPairs(allGear))
+              GearListing(gearPair: gear),
+        ],
       ),
     );
   }
