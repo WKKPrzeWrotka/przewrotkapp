8cc67093290e546e6da5f7369f57bc302e319c5e
Michał Markiewicz <michal.markiewicz@intive.com>
2025-11-07 09:17:09 +0100

    Update sign in method

--- przewrotkapp_flutter/lib/ui/pages/sign_in/sign_in_page.dart
+++ przewrotkapp_flutter/lib/ui/pages/sign_in/sign_in_page.dart
@@ -2,21 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:przewrotkapp_client/przewrotkapp_client.dart';
-import 'package:serverpod_auth_email_flutter/serverpod_auth_email_flutter.dart';
 
-class SignInPage extends StatelessWidget {
+import 'signin_dialog_custom.dart';
+
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Wait until the first frame is rendered before showing the dialog
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showSignInWithEmailDialog(
+        context: context,
+        caller: context.read<Client>().modules.auth,
+        initPage: InitPage.signIn,
+        onSignedIn: () => context.go('/'),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('PrzeZalogujSie')),
-      body: Center(
-        child: SignInWithEmailButton(
-          caller: context.read<Client>().modules.auth,
-          onSignedIn: () => context.go('/'),
-        ),
-      ),
-    );
+    return Scaffold(appBar: AppBar(title: Text('PrzeZalogujSie')));
   }
 }
