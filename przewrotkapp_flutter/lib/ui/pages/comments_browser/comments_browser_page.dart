ff293911c8ece117ac52cd29ad7003105daee430
TheLastGimbus <mateusz.soszynski@tuta.io>
2026-02-10 13:25:23 +0100

    comments sorting

--- przewrotkapp_flutter/lib/ui/pages/comments_browser/comments_browser_page.dart
+++ przewrotkapp_flutter/lib/ui/pages/comments_browser/comments_browser_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:przewrotkapp_client/przewrotkapp_client.dart';
 
 import '../../../logic/data_types.dart';
 import '../../common/comment_listing.dart';
@@ -7,16 +8,36 @@ import '../../common/comment_listing.dart';
 class CommentsBrowserPage extends StatelessWidget {
   const CommentsBrowserPage({super.key});
 
+  // Priority:
+  // - hours
+  // - severity
+  List<Comment> sortComments(List<Comment> comments) {
+    return comments.toList()..sort((a, b) {
+      final hours = (b.hoursForResolving ?? 0).compareTo(
+        a.hoursForResolving ?? 0,
+      );
+      if (hours != 0) return hours;
+      severityStrength(CommentType comment) => switch (comment) {
+        CommentType.neutral => 0,
+        CommentType.warning => 1,
+        CommentType.broken => 2,
+      };
+      final int severity = severityStrength(
+        b.type,
+      ).compareTo(severityStrength(a.type));
+      if (severity != 0) return severity;
+      return 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final comments = context.watch<UnresolvedComments?>();
+    final comments = context.select<UnresolvedComments?, List<Comment>?>(
+      (u) => u != null ? sortComments(u) : null,
+    );
     // TODO:
-    // - sorting
-    //   - options for sorting and filtering
-    //     - latest, most hours
     // - search bar?
     // - better listing for standalone
-    // - categories? big title "broken" that separate them from others, etc
     return Scaffold(
       appBar: AppBar(title: Text("Wszystkie komentarze 📃")),
       body: ListView.builder(
